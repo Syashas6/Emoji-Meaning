f0d65471201732dd0740e3130e24f0f43e50f6b8
Syashas6 <77928650+Syashas6@users.noreply.github.com>
2021-11-04 20:14:50 +0530

    Emoji-Pedia Modified version

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,17 @@
 import React from "react";
+import Card from "./Card";
+import Emojis from "../emojipedia";
+
+function CreateCard(Emoji) {
+  return (
+    <Card
+      id={Emoji.id}
+      emoji={Emoji.emoji}
+      name={Emoji.name}
+      meaning={Emoji.meaning}
+    />
+  );
+}
 
 function App() {
   return (
@@ -8,44 +21,25 @@ function App() {
       </h1>
 
       <dl className="dictionary">
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              💪
-            </span>
-            <span>Tense Biceps</span>
-          </dt>
-          <dd>
-            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-            used in connection with doing sports, e.g. at the gym.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🙏
-            </span>
-            <span>Person With Folded Hands</span>
-          </dt>
-          <dd>
-            Two hands pressed together. Is currently very introverted, saying a
-            prayer, or hoping for enlightenment. Is also used as a “high five”
-            or to say thank you.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🤣
-            </span>
-            <span>Rolling On The Floor, Laughing</span>
-          </dt>
-          <dd>
-            This is funny! A smiley face, rolling on the floor, laughing. The
-            face is laughing boundlessly. The emoji version of “rofl“. Stands
-            for „rolling on the floor, laughing“.
-          </dd>
-        </div>
+        {Emojis.map(CreateCard)}
+        {/* <Card
+          id= {Emojis[0].id}
+          emoji={Emojis[0].emoji} 
+          name = {Emojis[0].name}
+          meaning= {Emojis[0].meaning}
+        />
+        <Card
+          id= {Emojis[1].id}
+          emoji={Emojis[1].emoji} 
+          name = {Emojis[1].name}
+          meaning= {Emojis[1].meaning}
+        />
+        <Card
+          id= {Emojis[2].id}
+          emoji={Emojis[2].emoji} 
+          name = {Emojis[2].name}
+          meaning= {Emojis[2].meaning}
+        /> */}
       </dl>
     </div>
   );
